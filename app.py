821cb7f396040d36b5655c153cf3301c5a7c8277
Qureshi Faheem <qureshifaheem98.fq@gmail.com>
2024-01-21 23:42:05 +0530

    added a view function for index page and logic for guessed number validation.

--- app.py
+++ app.py
@@ -1,11 +1,44 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
+import random
 
 # intializing an app
 app = Flask(__name__)
+count = 0
 
-@app.route('/')
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return "Hello World!!"
+    if request.method == 'POST':
+        start = int(request.form['start'])
+        end = int(request.form['end'])
+        global x
+        x = random.randint(start, end)
+        return redirect("/guess")
+    else:
+        return render_template("range.html")
+
+
+@app.route('/guess', methods=['GET', 'POST'])
+def guess():
+    global x, count
+    print("x @index_function is: ", x)
+    message = "Start The Guess !!"
+    if request.method == 'POST':
+        user_guess = int(request.form['guess'])
+        if user_guess == x:
+            message = "You Guessed Right !!!"
+            return render_template("guess.html", message=message, count=count)
+        elif user_guess < x:
+            message = " Too Small Guess"
+            count += 1
+            return render_template("guess.html", message=message, count=count)
+        else:
+            message = "Too High Buddy !!!"
+            count += 1
+            return render_template("guess.html", message=message, count=count)
+    else:
+        return render_template("guess.html", message=message, count=count)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
